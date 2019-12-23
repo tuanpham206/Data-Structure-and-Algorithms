3917669a4fb123487ceb59226b41101082e3a7d4
Pham Anh Tuan <itviet@Energy.local>
2019-12-23 10:54:47 +0700

    update

---
+++ Linear/cmake-build-debug/CMakeFiles/Linear.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Linear.dir/main.cpp.o"
+  "Linear"
+  "Linear.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Linear.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
